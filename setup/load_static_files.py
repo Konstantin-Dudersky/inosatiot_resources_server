53e379cb4bdac17824add800b63fc561aef92d14
konstantin <konstantin.dudersky@gmail.com>
2021-08-24 12:16:20 +0300

    ability to set versions for bootswatch

--- setup/load_static_files.py
+++ setup/load_static_files.py
@@ -2,9 +2,14 @@
 Загрузка статических библиотек из интернета
 """
 
+import shutil
+import tarfile
 import urllib.request
 from pathlib import Path
+
+# set version
 bootstrap_version = '5.0.2'
+bootswatch_version = '5.0.2'
 
 
 def download_file(download_url, filename):
@@ -15,22 +20,35 @@ def download_file(download_url, filename):
     print(f"File loaded: {filename}")
 
 
-# js
+# ----------------------------------------------------------------------------------------------------------------------
+# bootstrap
 Path("main/static/js/bootstrap").mkdir(parents=True, exist_ok=True)
 
 download_file(f"https://cdn.jsdelivr.net/npm/bootstrap@{bootstrap_version}/dist/js/bootstrap.bundle.min.js",
               "main/static/js/bootstrap/bootstrap.bundle.min.js")
 
-# css
 Path("main/static/css/bootstrap").mkdir(parents=True, exist_ok=True)
 download_file(f"https://cdn.jsdelivr.net/npm/bootstrap@{bootstrap_version}/dist/css/bootstrap.min.css",
               "main/static/css/bootstrap/bootstrap.min.css")
 
-download_file(f"https://bootswatch.com/5/darkly/bootstrap.min.css",
-              "main/static/css/bootstrap/bootstrap.darkly.min.css")
+# ----------------------------------------------------------------------------------------------------------------------
 
-download_file(f"https://bootswatch.com/5/flatly/bootstrap.min.css",
-              "main/static/css/bootstrap/bootstrap.flatly.min.css")
+# bootswatch
 
-download_file(f"https://bootswatch.com/5/sandstone/bootstrap.min.css",
-              "main/static/css/bootstrap/bootstrap.sandstone.min.css")
+# load archive
+Path("tmp").mkdir(parents=True, exist_ok=True)
+download_file(f"https://github.com/thomaspark/bootswatch/archive/refs/tags/v{bootswatch_version}.tar.gz",
+              "tmp/bootswatch.tar.gz")
+
+# unpack
+tar = tarfile.open("tmp/bootswatch.tar.gz", "r")
+tar.extractall(path="tmp/")
+tar.close()
+
+# copy files
+for theme in ['darkly', 'flatly', 'sandstone']:
+    shutil.copy2(f"tmp/bootswatch-{bootswatch_version}/dist/{theme}/bootstrap.min.css",
+                 f"main/static/css/bootstrap/bootstrap.{theme}.min.css")
+
+# delete folder
+shutil.rmtree("tmp/")
